eacbdcec8c7102acd62c7fb11532ca1eda5efca0
Peter Judge <pjudge2002@gmail.com>
2022-05-14 16:59:04 -0400

    blah

--- forms.py
+++ forms.py
@@ -4,8 +4,4 @@ from wtforms.validators import InputRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
     email = EmailField("Email: ", validators=[InputRequired(), Email('Email must be in valid format')])
-    password = PasswordField("Password: ", 
-        validators=[InputRequired(), Length(min=8, max=256)])
-    confirm_password = PasswordField("Confirm Password: ", 
-        validators=[EqualTo('password', message='Passwords must match')])
     submit = SubmitField("Register")
