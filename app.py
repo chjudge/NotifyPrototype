07b4544c048efbdb3aa2893c21dc1b45671d40d4
chjudge <judgechj19@gcc.edu>
2022-05-14 16:01:28 -0400

    stealing forms

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from forms import RegisterForm
 
 app = Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'bf228mslOG748F7lmfusbgru'
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/register/')
+def get_register():
+    r_form = RegisterForm()
+    return render_template('register.html', form=r_form)
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,5 +1,12 @@
 from flask_wtf import FlaskForm
-
+from wtforms.fields import PasswordField, SubmitField, StringField, SelectField
+from wtforms.fields.html5 import EmailField, TelField
+from wtforms.validators import InputRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    pass
+    email = EmailField("Email: ", validators=[InputRequired(), Email('Email must be in valid format')])
+    password = PasswordField("Password: ", 
+        validators=[InputRequired(), Length(min=8, max=256)])
+    confirm_password = PasswordField("Confirm Password: ", 
+        validators=[EqualTo('password', message='Passwords must match')])
+    submit = SubmitField("Register")
