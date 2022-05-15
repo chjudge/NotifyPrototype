7cefb7ff45b25f1e0342c8e3da9d6e8ec1b84065
Peter Judge <pjudge2002@gmail.com>
2022-05-14 21:52:51 -0400

    added to form

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from typing import Text
 from flask_wtf import FlaskForm
-from wtforms.fields import (PasswordField, SubmitField, EmailField, TelField, StringField, 
-    SelectField, TelField, TextAreaField)
+from wtforms.fields import (PasswordField, SubmitField, EmailField, TelField, StringField,
+    SelectField, TelField, TextAreaField, IntegerField)
 from wtforms.validators import InputRequired, Email, EqualTo, Length
 
 STATES = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
@@ -16,4 +16,6 @@ class RegisterForm(FlaskForm):
     email = EmailField("Email: ", validators=[InputRequired(), Email('Email must be in valid format')])
     number = TelField("Mobile Phone Number: ", validators=[InputRequired(), Length(10)])
     state = SelectField("State: ", choices=STATES)
+    zip =  IntegerField("Zip Code: ", validators=[InputRequired(), Length(5)])
+    # Precinct 
     submit = SubmitField("Register")
